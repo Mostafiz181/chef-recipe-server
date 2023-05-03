c4c387c06e0b2749f80797d0a91933c805c444d3
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-05-03 18:55:39 +0600

    create a server and then fetch data from this api

---
+++ index.js
@@ -0,0 +1,30 @@
+const express= require('express')
+const app = express()
+const cors=require('cors')
+const port =process.env.PORT || 5000;
+
+const chefs=require("./data/chefs.json")
+
+app.use(cors())
+
+
+
+app.get('/',(req,res)=>{
+    res.send("chefs is running ")
+})
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
+})
+
+app.get('/chefs/:id', (req,res)=>{
+    const id=req.params.id;
+    console.log(id);
+    const selectedChef=chefs.find(s=>s.id=== id);
+    res.send(selectedChef)
+
+})
+
+app.listen(port,()=>{
+    console.log(`chefs APi is running of port: ${port}`)
+})
